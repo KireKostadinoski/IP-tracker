30bf9e3063c45c03d212bb39b9fe007d704a4c68
Kire Kostadinoski <k.kostadinoski@outlook.com>
2024-08-24 15:08:31 +0200

    uploading app

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function updateMapAndInfo(data) {
 }
 
 // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 10);
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
